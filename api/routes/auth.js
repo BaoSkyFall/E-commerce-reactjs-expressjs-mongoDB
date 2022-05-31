d6e85f45708cf52c28c43f674903370e24292cf5
BaoSkyFall <39408162+BaoSkyFall@users.noreply.github.com>
2022-05-31 17:46:51 +0700

    add valid auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,23 +5,30 @@ const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
-  });
-
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    console.log('err:', err)
-    res.status(500).json(err);
+  console.log('req.body:', req.body)
+  if (req.body.username && req.body.password) {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
+    });
+    try {
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
+      console.log('err:', err)
+      res.status(500).json(err);
+    }
   }
+  else {
+    res.status(500).json("Not have body");
+
+  }
+
+
 });
 
 //LOGIN
