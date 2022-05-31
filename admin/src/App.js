6e341e414b0dc8bdecb56b874e620087c9c9102c
BaoSkyFall <39408162+BaoSkyFall@users.noreply.github.com>
2022-05-31 17:13:53 +0700

    add file env

--- admin/src/App.js
+++ admin/src/App.js
@@ -18,7 +18,7 @@ import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
 
 function App() {
-  const admin = useSelector((state) => state.user.currentUser ? state.user.currentUser.isAdmin : true);
+  const admin = useSelector((state) => state.user.currentUser ? state.user.currentUser.isAdmin : false);
   return (
     <Router>
       <Switch>
